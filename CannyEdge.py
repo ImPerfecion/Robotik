092ada3e67ab78fb6bf11cd4291ff33f70db6897
Daniel J. Bergmann <Danieljensbergmann@outlook.de>
2021-01-11 10:25:22 +0100

    update everything(kinda messy)

--- CannyEdge.py
+++ CannyEdge.py
@@ -1,1 +1,4 @@
-#Here will be the Canny Edge detection algorithm from https://www.youtube.com/watch?v=e
+#Here will be the Canny Edge detection algorithm from https://www.youtube.com/watch?v=eLTLtUVuuy4
+
+import cv2
+import numpy as np
